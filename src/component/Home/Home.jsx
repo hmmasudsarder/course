71c3b4f703426765a6f8ade129e6cb155e0b13fb
hmdmasudsarder <hmmasudsarder@gmail.com>
2023-09-15 21:01:08 +0600

    change json file

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -45,7 +45,7 @@ const Home = () => {
                             </div>
                             <h3>{course.name}</h3>
                             <p>
-                              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                              {course.description}
                             </p>
                             <div className="info">
                               <div className="icons">
